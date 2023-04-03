a8242c2b659118d068d6a0fba8d6d6eb5a4f4d86
Mary-Clb <mbressy@teclib.com>
2023-04-03 17:02:31 +0200

    fix custom scss + try finBy() on db

--- src/Controller/TelemetryController.php
+++ src/Controller/TelemetryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TelemetryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class TelemetryController extends AbstractController
 {
     #[Route('/telemetry', name: 'app_telemetry')]
-    public function index(): Response
+    public function index(TelemetryRepository $telemetryRepository): Response
     {
+        $data = $telemetryRepository->findBy([], ['id' => 'ASC'], 35, 0);
         return $this->render('telemetry/index.html.twig', [
-            'controller_name' => 'TelemetryController',
+            'controller_name' => 'controller-name',
+            'datas'           => $data
+
         ]);
     }
 }
