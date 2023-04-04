3efb5e16f8159453186cc4900681497360c865f0
Mary-Clb <mbressy@teclib.com>
2023-04-04 15:38:18 +0200

    install & test echarts

--- assets/app.js
+++ assets/app.js
@@ -4,14 +4,16 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 import 'bootstrap';
 import bsCustomFileInput from 'bs-custom-file-input';
-
 // start the Stimulus application
 import './bootstrap';
 
 bsCustomFileInput.init();
+
+import './echarts/charts';
+
+
 
