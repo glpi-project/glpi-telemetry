eefcf54fe574aff3352bf1a7107f048404d84501
Mary-Clb <mbressy@teclib.com>
2023-07-11 00:54:56 +0200

    add daterange front

--- assets/app.js
+++ assets/app.js
@@ -7,8 +7,17 @@
 import * as echarts from 'echarts';
 window.echarts=echarts;
 
-import { easepick } from '@easepick/bundle';
+import { easepick } from '@easepick/core';
 global.easepick = easepick;
+
+import { AmpPlugin } from '@easepick/amp-plugin';
+global.AmpPlugin = AmpPlugin;
+
+import { RangePlugin } from '@easepick/range-plugin';
+global.RangePlugin = RangePlugin;
+
+import { PresetPlugin } from '@easepick/bundle';
+global.PresetPlugin = PresetPlugin;
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 import 'bootstrap';
