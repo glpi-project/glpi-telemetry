439c5d1cec24727776249964a30f632bedaf656a
Mary-Clb <mbressy@teclib.com>
2023-10-05 12:13:38 +0200

    update register modal, toggler btn + margin

--- src/Form/ReferenceFormType.php
+++ src/Form/ReferenceFormType.php
@@ -6,7 +6,6 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
@@ -18,15 +17,15 @@ class ReferenceFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class)
-            ->add('url', UrlType::class)
-            ->add('country', CountryType::class)
-            ->add('phone', TelType::class)
-            ->add('email', EmailType::class)
-            ->add('referent', TextType::class)
-            ->add('nb_assets')
-            ->add('nb_helpdesk')
-            ->add('comment', TextareaType::class, ['attr' => ['rows' => 6]])
+            ->add('name', TextType::class, ['label' => false])
+            ->add('url', UrlType::class, ['label' => false])
+            ->add('country', CountryType::class, ['label' => false])
+            ->add('phone', TelType::class, ['label' => false])
+            ->add('email', EmailType::class, ['label' => false])
+            ->add('referent', TextType::class, ['label' => false])
+            ->add('nb_assets', null, ['label' => false])
+            ->add('nb_helpdesk', null, ['label' => false])
+            ->add('comment', TextareaType::class, ['attr' => ['rows' => 6], 'label' => false])
         ;
     }
 
