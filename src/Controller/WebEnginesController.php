3222e53a64036edfcf017c24bc9b83b3dbbcfb11
Mary-Clb <mbressy@teclib.com>
2023-11-13 11:20:22 +0100

    modify sql request for web engines data

--- src/Controller/WebEnginesController.php
+++ src/Controller/WebEnginesController.php
@@ -5,12 +5,19 @@ namespace App\Controller;
 use App\Interface\ViewControllerInterface;
 use App\Repository\TelemetryRepository;
 use App\Service\RefreshCacheService;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 class WebEnginesController extends AbstractController implements ViewControllerInterface
 {
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
     #[Route('/web/engines', name: 'app_web_engines')]
 
     public function index(Request $request, RefreshCacheService $refreshCacheService): JsonResponse
@@ -29,7 +36,23 @@ class WebEnginesController extends AbstractController implements ViewControllerI
         $endDate        = $Dateparams['endDate'];
 
         $data = $telemetryRepository->getWebEngines($startDate, $endDate);
+        $chartData = $this->prepareChartData($data);
 
-        return $data;
+        return $chartData;
+    }
+
+    public function prepareChartData(array $data): array
+    {
+        $chartData = [];
+
+        foreach ($data as $entry) {
+            $chartData[] = [
+            'name'  => $entry['webengine'],
+            'value' => $entry['nb_instance'],
+            ];
+        }
+
+        $this->logger->info('chartData :', $chartData);
+        return $chartData;
     }
 }
