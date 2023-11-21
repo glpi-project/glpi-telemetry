fb1e8d7bd8b09ba0e7830411cf7daedd6f15e6ec
Mary-Clb <mbressy@teclib.com>
2023-11-21 17:44:57 +0100

    add install mode chart

---
+++ src/Controller/InstallModeController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use App\Interface\ViewControllerInterface;
+use App\Repository\TelemetryRepository;
+use App\Service\RefreshCacheService;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+class InstallModeController extends AbstractController implements ViewControllerInterface
+{
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    #[Route('/install/mode', name: 'app_install_mode')]
+    public function index(Request $request, RefreshCacheService $refreshCacheService): JsonResponse
+    {
+        $filter         = $request->query->get('filter');
+        $forceUpdate    = false;
+
+        $result = $refreshCacheService->refreshCache($filter, $forceUpdate, $this);
+
+        return $this->json($result);
+    }
+
+    public function getData(array $Dateparams, TelemetryRepository $telemetryRepository): array
+    {
+        $startDate      = $Dateparams['startDate'];
+        $endDate        = $Dateparams['endDate'];
+
+        $data = $telemetryRepository->getInstallMode($startDate, $endDate);
+        $chartData = $this->prepareChartData($data);
+
+        return $chartData;
+    }
+
+    public function prepareChartData(array $data): array
+    {
+        $chartData = [];
+
+        foreach ($data as $entry) {
+            $chartData[] = [
+            'name'  => $entry['mode'],
+            'value' => $entry['nb_instances'],
+            ];
+        }
+
+        $this->logger->info('chartData :', $chartData);
+        return $chartData;
+    }
+}
