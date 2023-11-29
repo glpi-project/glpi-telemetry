27df784ceb924e25d4a3695c66fb82dfdb085ab6
Cédric Anne <cedric.anne@gmail.com>
2023-11-29 08:40:24 +0100

    `echarts.js` script is actually not used

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,6 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-    .addEntry('echarts', './node_modules/echarts/dist/echarts.min.js')
     .addEntry('dashboard', './assets/dashboard.js')
     .addEntry('reference', './assets/reference.js')
 
