e2c2f919bf06b466e554608185ad18548d589bed
Mary-Clb <mbressy@teclib.com>
2023-11-30 14:45:42 +0100

    add captcha on reference form

--- src/Controller/ReferenceController.php
+++ src/Controller/ReferenceController.php
@@ -11,11 +11,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 class ReferenceController extends AbstractController
 {
     #[Route('/reference', name: 'app_reference')]
-    public function index(ReferenceRepository $referenceRepository, Request $request, EntityManagerInterface $manager): Response
+    public function index(ReferenceRepository $referenceRepository, Request $request, EntityManagerInterface $manager, HttpClientInterface $client): Response
     {
         $references = $referenceRepository->findAll();
         $nb = count($references);
@@ -23,28 +23,69 @@ class ReferenceController extends AbstractController
         $reference = new Reference();
         $glpi_reference = new GlpiReference();
 
+        $captchaSiteKey     = $this->getParameter('captcha.site_key');
+        $captchaSecretKey   = $this->getParameter('captcha.secret_key');
+
         $form = $this->createForm(ReferenceFormType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $captcha_token = $request->request->get('captcha_token');
 
-            $data = $form->getData();
+            if ($captcha_token === null) {
+                $captcha_is_ok = false;
+            } else {
+                try {
+                    $response = $client->request(
+                        'POST',
+                        'https://www.google.com/recaptcha/api/siteverify',
+                        [
+                            'headers' => [
+                                'Content-Type' => 'application/json',
+                            ],
+                            'body' => [
+                                'secret' => $captchaSecretKey,
+                                'response' => $request->request->get('captcha_token'),
+                            ]
+                        ]
+                    );
 
-            $reference->setName($data['name']);
-            $reference->setUrl($data['url']);
-            $reference->setCountry(strtolower($data['country']));
-            $reference->setPhone($data['phone']);
-            $reference->setEmail($data['email']);
-            $reference->setReferent($data['referent']);
-            $reference->setComment($data['comment']);
-            $reference->setCreatedAt(new \DateTimeImmutable());
-            $glpi_reference->setNumAssets($data['nb_assets']);
-            $glpi_reference->setNumHelpdesk($data['nb_helpdesk']);
-            $glpi_reference->setReference($reference);
-            $glpi_reference->setCreatedAt(new \DateTimeImmutable());
+                    $captcha_is_ok = $response->toArray()['success'] ?? false;
+                } catch (\Throwable $e) {
+                    $captcha_is_ok = true;
+                }
+            }
 
-            $manager->persist($glpi_reference);
-            $manager->flush();
+            $success = false;
+            if ($captcha_is_ok) {
+                try {
+                    $data = $form->getData();
+
+                    $reference->setName($data['name']);
+                    $reference->setUrl($data['url']);
+                    $reference->setCountry(strtolower($data['country']));
+                    $reference->setPhone($data['phone']);
+                    $reference->setEmail($data['email']);
+                    $reference->setReferent($data['referent']);
+                    $reference->setComment($data['comment']);
+                    $reference->setCreatedAt(new \DateTimeImmutable());
+                    $glpi_reference->setNumAssets($data['nb_assets']);
+                    $glpi_reference->setNumHelpdesk($data['nb_helpdesk']);
+                    $glpi_reference->setReference($reference);
+                    $glpi_reference->setCreatedAt(new \DateTimeImmutable());
+
+                    $manager->persist($glpi_reference);
+                    $manager->flush();
+                } catch (\Throwable $e) {
+                    $success = false;
+                }
+            }
+
+            if ($success) {
+                $this->addFlash('success', 'Your reference has been added successfully');
+            } else {
+                $this->addFlash('error', 'An error occurred while adding your reference');
+            }
 
             return $this->redirectToRoute('app_reference');
 
@@ -55,7 +96,8 @@ class ReferenceController extends AbstractController
             [
                 'references'   => $references,
                 'nb_ref'       => $nb,
-                'form'         => $form
+                'form'         => $form,
+                'captchaSiteKey' => $captchaSiteKey
             ]
         );
     }
