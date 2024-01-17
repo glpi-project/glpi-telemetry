49958842bb588425191ffda30874426cfad19e37
Marie Bressy <75746914+Mary-Clb@users.noreply.github.com>
2024-01-17 16:28:53 +0100

    Create chart zoom modals dynamically
    closes #143

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -42,4 +42,47 @@ window.addEventListener("DOMContentLoaded", () => {
         }
         global.executeFilterCallbacks(params);
     }
+
+    const modalButtons = document.querySelectorAll('.openModal');
+    modalButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+            const chartContainer = button.closest('.card').querySelector('.card-body');
+            const chart = echarts.getInstanceByDom(chartContainer);
+            const options = chart.getOption();
+            const title = typeof(options.title) !== 'undefined' && typeof(options.title[0]) !== 'undefined' && typeof(options.title[0].text) !== 'undefined'
+                ? options.title[0].text
+                : '';
+
+            const modal = document.createElement('div');
+            modal.setAttribute('class', 'modal modal-blur fade');
+            modal.setAttribute('role', 'dialog');
+            modal.innerHTML = `
+                <div class="modal-dialog modal-xl">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">${title}</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="chart-container" style="width: 100%; height: 80vh; max-height: 700px;"></div>
+                        </div>
+                    </div>
+                </div>
+            `;
+
+            modal.addEventListener('shown.bs.modal', () => {
+                options.title = {show:false};
+
+                const modalChartContainer = modal.querySelector('.chart-container');
+                const modalChart = echarts.init(modalChartContainer);
+                modalChart.setOption(options);
+            });
+            modal.addEventListener('hidden.bs.modal', () => {
+                modal.remove();
+            });
+
+            const bootstrapModal = new window.bootstrap.Modal(modal);
+            bootstrapModal.show();
+        });
+    });
 });
