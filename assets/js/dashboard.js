8aa89b1064e042335e2c35338fa69d10e2237cfa
Marie Bressy <75746914+Mary-Clb@users.noreply.github.com>
2024-01-22 10:28:17 +0100

    Simplify dashboard filters code
    closes #142

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,12 +1,8 @@
 import * as echarts from "echarts";
-import moment from "moment";
 
 window.echarts = echarts;
 
 global.filterCallbacks = [];
-global.results = [];
-global.clearResults = function() {global.results.length = 0; };
-global.registerResults = function (response) {global.results.push(response);};
 global.registerFilterCallback = function (callback) {global.filterCallbacks.push(callback);};
 global.executeFilterCallbacks = function (filters) {
     global.filterCallbacks.forEach(callback => {
@@ -16,30 +12,16 @@ global.executeFilterCallbacks = function (filters) {
 
 window.addEventListener("DOMContentLoaded", () => {
 
-    var select = document.getElementById('dataPeriod');
-    var selectedOption = select.value;
-    var today = moment().format('Y-MM-DD hh:mm:ss');
-    var defaultStartDate = moment().subtract(1, 'year').format('Y-MM-DD hh:mm:ss');
+    let select = document.getElementById('dataPeriod');
+    let selectedOption = select.value;
     var params = {};
     setValue(selectedOption);
 
-    select.onchange = function() {setValue();};
+    select.onchange = function () {setValue();};
 
     function setValue() {
         selectedOption = document.getElementById('dataPeriod').value;
-        switch (selectedOption) {
-            case 'lastYear':
-                params = {startDate: defaultStartDate, endDate: today, filter: selectedOption};
-                break;
-            case 'fiveYear':
-                defaultStartDate = moment().subtract(5, 'years').format('Y-MM-DD hh:mm:ss');
-                params = {startDate: defaultStartDate, endDate: today, filter: selectedOption};
-                break;
-            case 'always':
-                defaultStartDate = moment().subtract(10, 'years').format('Y-MM-DD hh:mm:ss');
-                params = {startDate: defaultStartDate, endDate: today, filter: selectedOption};
-                break;
-        }
+        params = { filter: selectedOption };
         global.executeFilterCallbacks(params);
     }
 
