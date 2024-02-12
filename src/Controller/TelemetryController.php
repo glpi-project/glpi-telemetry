5270285cd7f7e3f440020f2f3b64b12ae3111694
Cédric Anne <cedric.anne@gmail.com>
2024-02-12 13:40:22 +0100

    sorted imports

--- src/Controller/TelemetryController.php
+++ src/Controller/TelemetryController.php
@@ -8,8 +8,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Component\Routing\Annotation\Route;
 
 class TelemetryController extends AbstractController
 {
