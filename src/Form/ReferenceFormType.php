faf7f3a09170c3aded56ba80685df70f8e2a5313
Cédric Anne <cedric.anne@gmail.com>
2024-02-19 13:22:38 +0100

    Fix assets/helpdesk data type

--- src/Form/ReferenceFormType.php
+++ src/Form/ReferenceFormType.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace App\Form;
 
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 
@@ -25,8 +25,8 @@ class ReferenceFormType extends AbstractType
             ->add('phone', TelType::class, ['label' => false, 'required' => false, 'attr' => ['placeholder' => 'Phone']])
             ->add('email', EmailType::class, ['label' => false,'required' => false, 'attr' => ['placeholder' => 'Email']])
             ->add('referent', TextType::class, ['label' => false, 'required' => false, 'attr' => ['placeholder' => 'Referent name']])
-            ->add('nb_assets', NumberType::class, ['label' => false, 'required' => false, 'attr' => ['placeholder' => 'Number of assets', 'step' => 1]])
-            ->add('nb_helpdesk', NumberType::class, ['label' => false, 'required' => false, 'attr' => ['placeholder' => 'Number of helpdesk', 'step' => 1]])
+            ->add('nb_assets', IntegerType::class, ['label' => false, 'required' => false, 'attr' => ['placeholder' => 'Number of assets', 'step' => 1]])
+            ->add('nb_helpdesk', IntegerType::class, ['label' => false, 'required' => false, 'attr' => ['placeholder' => 'Number of helpdesk', 'step' => 1]])
             ->add('comment', TextareaType::class, ['label' => false, 'required' => false, 'attr' => ['rows' => 6, 'placeholder' => 'Your message']])
         ;
     }
