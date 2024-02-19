5b2563c84fc44260a0a9d8db2b1bd7c88ee30f7c
Marie Bressy <75746914+Mary-Clb@users.noreply.github.com>
2024-02-19 12:02:36 +0100

    update issue #151 about adding references

--- src/Form/ReferenceFormType.php
+++ src/Form/ReferenceFormType.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Form;
 
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CountryType;
@@ -18,15 +19,15 @@ class ReferenceFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class, ['label' => false, 'required' => true])
-            ->add('url', UrlType::class, ['label' => false])
-            ->add('country', CountryType::class, ['label' => false, 'required' => true])
-            ->add('phone', TelType::class, ['label' => false])
-            ->add('email', EmailType::class, ['label' => false, 'required' => true])
-            ->add('referent', TextType::class, ['label' => false, 'required' => true])
-            ->add('nb_assets', null, ['label' => false, 'required' => true])
-            ->add('nb_helpdesk', null, ['label' => false, 'required' => true])
-            ->add('comment', TextareaType::class, ['attr' => ['rows' => 6], 'label' => false])
+            ->add('name', TextType::class, ['label' => false, 'required' => true, 'attr' => ['placeholder' => 'Your (company) name']])
+            ->add('url', UrlType::class, ['label' => false, 'attr' => ['placeholder' => 'URL']])
+            ->add('country', CountryType::class, ['label' => false, 'placeholder' => 'Choose a country'])
+            ->add('phone', TelType::class, ['label' => false, 'attr' => ['placeholder' => 'Phone']])
+            ->add('email', EmailType::class, ['label' => false, 'attr' => ['placeholder' => 'Email']])
+            ->add('referent', TextType::class, ['label' => false, 'attr' => ['placeholder' => 'Referent name']])
+            ->add('nb_assets', NumberType::class, ['label' => false, 'attr' => ['placeholder' => 'Number of assets', 'step' => 1]])
+            ->add('nb_helpdesk', NumberType::class, ['label' => false, 'attr' => ['placeholder' => 'Number of helpdesk', 'step' => 1]])
+            ->add('comment', TextareaType::class, ['label' => false, 'attr' => ['rows' => 6, 'placeholder' => 'Your message']])
         ;
     }
 }
