2de270c0ff56930f82b247a2eaca170457d788bb
Cédric Anne <cedric.anne@gmail.com>
2024-02-19 10:54:43 +0100

    Ensure strict types are used

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -6,8 +6,10 @@ $finder = (new PhpCsFixer\Finder())
 ;
 
 return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
     ->setRules([
-        '@PER-CS2.0' => true,
+        '@PER-CS2.0'           => true,
+        'declare_strict_types' => true,
     ])
     ->setFinder($finder)
 ;
--- tests/Service/TelemetryDenormalizerTest.php
+++ tests/Service/TelemetryDenormalizerTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Entity\GlpiPlugin;
 use App\Entity\Telemetry;
 use App\Entity\TelemetryGlpiPlugin;
