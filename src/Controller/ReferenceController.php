35357712a5792aa501414f7f9281c7552fe9805d
Cédric Anne <cedric.anne@gmail.com>
2024-03-04 12:21:53 +0100

    remove import

--- src/Controller/ReferenceController.php
+++ src/Controller/ReferenceController.php
@@ -14,7 +14,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Psr\Log\LoggerInterface;
 
 class ReferenceController extends AbstractController
 {
