b5e25473d3804e416bd648b553a0ed442979bd18
Mary-Clb <mbressy@teclib.com>
2024-03-07 16:21:13 +0100

    move functions for bar charts in AbstractChartController

--- src/Controller/AbstractChartController.php
+++ src/Controller/AbstractChartController.php
@@ -59,4 +59,141 @@ abstract class AbstractChartController extends AbstractController
 
         return $chartData;
     }
+
+        /**
+     * @param array<array<string,mixed>> $data
+     * @return array{
+     *   periods: array<int,string>,
+     *   versions: array<int,string>,
+     *   data: array<string,array<string,int>>
+     * } $transformedData
+     */
+    public function prepareDataForBarChart(array $data, string $controllerName): array
+    {
+        $periods = [];
+        $versions = [];
+        $result = [];
+
+        // Step 1: Loop through the data to fill the $periods and $versions arrays
+        foreach ($data as $monthYear => $entries) {
+            // Add the period to the $periods array if it's not already there
+            if (!in_array($monthYear, $periods)) {
+                $periods[] = $monthYear;
+            }
+
+            foreach ($entries as $entry) {
+                $version = $entry['name'];
+                $nbInstance = $entry['total'];
+
+                if (str_contains($controllerName, 'glpiversion')) {
+                    if (preg_match('/^(9|10)\.\d+$/', $version) !== 1) {
+                        // ignore invalid versions (e.g. `1.6`, `V1`, ...)
+                        // TODO Validate version against Github releases when telemetry data is received
+                        continue;
+                    }
+                }
+
+                // Add the version to the $versions array if it's not already there
+                if (!in_array($version, $versions)) {
+                    $versions[] = $version;
+                }
+
+                // Add the users to the corresponding period and version
+                $result[$monthYear][$version] = $nbInstance;
+            }
+        }
+
+        // Step 2: Sort the periods
+        usort($periods, function ($a, $b) {
+            return strtotime($a) - strtotime($b);
+        });
+
+        // Step 3: Sort the versions
+        sort($versions);
+
+        // Step 4: Fill in missing values with 0
+        foreach ($periods as $period) {
+            foreach ($versions as $version) {
+                if (!isset($result[$period][$version])) {
+                    $result[$period][$version] = 0;
+                }
+            }
+            // Sort the versions within each period
+            ksort($result[$period]);
+        }
+
+        $preparedData = [
+            'periods' => $periods,
+            'versions' => $versions,
+            'data' => $result
+        ];
+
+        return $this->prepareChartData($preparedData);
+    }
+
+    /**
+     * @param array{
+     *     periods: array<int, string>,
+     *     versions: array<int, string>,
+     *     data: array<string, array<string, int>>
+     * } $transformedData
+     * @return array{
+     *     xAxis: array{
+     *         data: array<int, string>
+     *     },
+     *     series: array<int, array{
+     *         name: string,
+     *         type: string,
+     *         stack: string,
+     *         label: array{
+     *             show: bool
+     *         },
+     *         emphasis: array{
+     *             focus: string
+     *         },
+     *         data: array<int, float|int>
+     *     }>
+     * }
+     */
+    public function prepareChartData(array $transformedData): array
+    {
+        $chartData = [
+            'xAxis' => [
+                'data' => $transformedData['periods']
+            ],
+            'series' => []
+        ];
+
+        // Calculer le total des instances pour chaque période
+        $totalInstancesPerPeriod = [];
+        foreach ($transformedData['data'] as $period => $versions) {
+            $totalInstancesPerPeriod[$period] = array_sum($versions);
+        }
+
+        foreach ($transformedData['versions'] as $version) {
+            $seriesData = [
+                'name' => $version,
+                'type' => 'bar',
+                'stack' => 'total',
+                'label' => [
+                    'show' => false,
+                ],
+                'emphasis' => [
+                    'focus' => 'series',
+                ],
+                'data' => []
+            ];
+
+            foreach ($transformedData['periods'] as $period) {
+                $percentage = $totalInstancesPerPeriod[$period] > 0
+                    ? round(($transformedData['data'][$period][$version] / $totalInstancesPerPeriod[$period]) * 100, 2)
+                    : 0;
+                $seriesData['data'][] = $percentage;
+            }
+
+            $chartData['series'][] = $seriesData;
+        }
+
+        return $chartData;
+    }
 }
--- src/Controller/PhpVersionController.php
+++ src/Controller/PhpVersionController.php
@@ -18,156 +18,15 @@ class PhpVersionController extends AbstractChartController
     {
         $filter         = $request->query->get('filter');
         $period         = $this->getPeriodFromFilter($filter);
+        $controllerName = 'PhpVersionController';
 
         $start          = new \DateTime($period['startDate']);
         $end            = new \DateTime($period['endDate']);
 
         $res = $chartDataStorage->getMonthlyValues(ChartSerie::PhpInfos, $start, $end);
 
-        $result = $this->prepareDataForPieChart($res);
+        $result = $this->prepareDataForBarChart($res, $controllerName);
 
         return new JsonResponse($result);
-    }
-
-    /**
-     * @param array<array<string,mixed>> $data
-     * @return array<array<string,mixed>>
-     */
-    public function processData(array $data): array
-    {
-        $transformedData   = $this->transformDataForChart($data);
-        $chartData         = $this->prepareChartData($transformedData);
-
-        return $chartData;
-    }
-
-    /**
-     * @param array<array<string,mixed>> $data
-     * @return array{
-     *     periods: array<int,string>,
-     *     versions: array<int,string>,
-     *     data: array<string,array<string,int>>
-     * } $transformedData
-     */
-    public function transformDataForChart(array $data): array
-    {
-        $periods = [];
-        $versions = [];
-        $result = [];
-
-        // Step 1: Loop through the data to fill the $periods and $versions arrays
-        foreach ($data as $entry) {
-            $monthYear = $entry['month_year'];
-            $version = $entry['version'];
-            $nbInstance = $entry['nb_instance'];
-
-            // Add the period to the $periods array if it's not already there
-            if (!in_array($monthYear, $periods)) {
-                $periods[] = $monthYear;
-            }
-
-            // Add the version to the $versions array if it's not already there
-            if (!in_array($version, $versions)) {
-                $versions[] = $version;
-            }
-
-            /// Initialize the array for the period if necessary
-            if (!isset($result[$monthYear])) {
-                $result[$monthYear] = [];
-            }
-
-            // Add the users to the corresponding period and version
-            $result[$monthYear][$version] = $nbInstance;
-        }
-
-        // Étape 2 : Trier les périodes
-        usort($periods, function ($a, $b) {
-            return strtotime($a) - strtotime($b);
-        });
-
-        // Step 2: Sort the periods
-        sort($versions);
-
-        // Step 4: Fill in missing values with 0
-        foreach ($periods as $period) {
-            foreach ($versions as $version) {
-                if (!isset($result[$period][$version])) {
-                    $result[$period][$version] = 0;
-                }
-            }
-            // Sort the versions within each period
-            ksort($result[$period]);
-        }
-        return [
-            'periods' => $periods,
-            'versions' => $versions,
-            'data' => $result
-        ];
-    }
-
-    /**
-     * @param array{
-     *     periods: array<int, string>,
-     *     versions: array<int, string>,
-     *     data: array<string, array<string, int>>
-     * } $transformedData
-     * @return array{
-     *     xAxis: array{
-     *         data: array<int, string>
-     *     },
-     *     series: array<int, array{
-     *         name: string,
-     *         type: string,
-     *         stack: string,
-     *         label: array{
-     *             show: bool
-     *         },
-     *         emphasis: array{
-     *             focus: string
-     *         },
-     *         data: array<int, float|int>
-     *     }>
-     * }
-     */
-    public function prepareChartData(array $transformedData): array
-    {
-        $chartData = [
-            'xAxis' => [
-                'data' => $transformedData['periods']
-            ],
-            'series' => []
-        ];
-
-        // Calculer le total des instances pour chaque période
-        $totalInstancesPerPeriod = [];
-        foreach ($transformedData['data'] as $period => $versions) {
-            $totalInstancesPerPeriod[$period] = array_sum($versions);
-        }
-
-        foreach ($transformedData['versions'] as $version) {
-            $seriesData = [
-                'name' => $version,
-                'type' => 'bar',
-                'stack' => 'total',
-                'label' => [
-                    'show' => false,
-                ],
-                'emphasis' => [
-                    'focus' => 'series',
-                ],
-                'data' => []
-            ];
-
-            foreach ($transformedData['periods'] as $period) {
-                $percentage = $totalInstancesPerPeriod[$period] > 0
-                    ? round(($transformedData['data'][$period][$version] / $totalInstancesPerPeriod[$period]) * 100, 2)
-                    : 0;
-                $seriesData['data'][] = $percentage;
-            }
-
-            $chartData['series'][] = $seriesData;
-        }
-
-        return $chartData;
     }
 }
