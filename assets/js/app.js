d29ea56bd5cd394f3a97f2cee9685e3bedbe0910
Cédric Anne <cedric.anne@gmail.com>
2024-05-02 12:25:33 +0200

    Remove useless embedding in `DOMContentLoaded` event listener

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,45 +5,43 @@ import * as CookieConsent from "vanilla-cookieconsent";
 window.echarts = echarts;
 
 // Cookie consent banner
-window.addEventListener("DOMContentLoaded", () => {
-    CookieConsent.run({
-        categories: {
-            analytics: {
-                services: {
-                    ga: {
-                        label: 'Google Analytics',
-                        onAccept: () => {
-                            var gaScript = document.createElement('script');
-                            gaScript.async = true;
-                            gaScript.src = `https://www.googletagmanager.com/gtag/js?id=${window.google_analytics_id}`;
-                            document.head.appendChild(gaScript);
-                        },
-                        onReject: () => {
-                            window[`ga-disable-${window.google_analytics_id}`] = true;
-                        },
-                        cookies: [
-                            {
-                                name: /^(_ga|_gid)/
-                            }
-                        ]
+CookieConsent.run({
+    categories: {
+        analytics: {
+            services: {
+                ga: {
+                    label: 'Google Analytics',
+                    onAccept: () => {
+                        var gaScript = document.createElement('script');
+                        gaScript.async = true;
+                        gaScript.src = `https://www.googletagmanager.com/gtag/js?id=${window.google_analytics_id}`;
+                        document.head.appendChild(gaScript);
                     },
-                }
+                    onReject: () => {
+                        window[`ga-disable-${window.google_analytics_id}`] = true;
+                    },
+                    cookies: [
+                        {
+                            name: /^(_ga|_gid)/
+                        }
+                    ]
+                },
             }
-        },
+        }
+    },
 
-        language: {
-            default: 'en',
-            translations: {
-                en: {
-                    consentModal: {
-                        title: 'Welcome to the GLPI telemetry website',
-                        description: "We use cookies to collect information about how you use our website. By clicking 'Accept', you agree to the use of cookies.",
-                        acceptAllBtn: 'Accept',
-                        acceptNecessaryBtn: 'Reject',
-                        showPreferencesBtn: null
-                    }
+    language: {
+        default: 'en',
+        translations: {
+            en: {
+                consentModal: {
+                    title: 'Welcome to the GLPI telemetry website',
+                    description: "We use cookies to collect information about how you use our website. By clicking 'Accept', you agree to the use of cookies.",
+                    acceptAllBtn: 'Accept',
+                    acceptNecessaryBtn: 'Reject',
+                    showPreferencesBtn: null
                 }
             }
         }
-    });
+    }
 });
