730aa7d164d48a8f3414dc76b19b74e653ea46c1
Cédric Anne <cedric.anne@gmail.com>
2024-05-02 14:30:18 +0200

    Nettoyage de code mort (#298)
    Clean useless/dead code
    
    * Remove useless override of docker default command
    * Remove deprecated version in docker-compose.yml
    * Remove useless embedding in `DOMContentLoaded` event listener
    * Do not catch exception when no feedback is given to users
    * Remove unused styles
    * Clean `.gitignore` files
    * Clean dead codein `App\Repository\TelemetryRepository`

--- assets/js/reference.js
+++ assets/js/reference.js
@@ -47,172 +47,166 @@ const displayMapInModal = function () {
     bootstrapModal.show();
 };
 
-window.addEventListener('DOMContentLoaded', () => {
-    const mapChartContainer = document.querySelector('.reference-map');
-    const mapChart = global.echarts.init(mapChartContainer);
+// Initialize map
+const mapChartContainer = document.querySelector('.reference-map');
+const mapChart = global.echarts.init(mapChartContainer);
 
-    mapChart.showLoading();
+mapChart.showLoading();
 
-    fetch('reference/map/countries', {
+fetch('reference/map/countries', {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+}).then(response => {
+    return response.json();
+}).then(geoJson => {
+    mapChart.hideLoading();
+    global.echarts.registerMap('countriesMap', geoJson);
+
+    mapChart.setOption({
+        title: {
+            text: 'GLPI references by country',
+            left: 'center',
+        },
+        tooltip: {
+            formatter: '{b}: {c}'
+        },
+        visualMap: {
+            show: false,
+            min: 1,
+            max: 1000,
+            inRange: {
+                color: [
+                    '#e6f7ff',
+                    '#bae7ff',
+                    '#91d5ff',
+                    '#69c0ff',
+                    '#40a9ff',
+                    '#1890ff',
+                    '#096dd9',
+                    '#0050b3',
+                    '#003a8c',
+                    '#002766',
+                    '#00134d'
+                ]
+            },
+        },
+        series: [
+            {
+                type: 'map',
+                roam: false,
+                map: 'countriesMap',
+                emphasis: {
+                    label: {
+                        show: false
+                    }
+                },
+                select: {
+                    disabled: true,
+                },
+                data: []
+            }
+        ]
+    });
+
+    fetch('reference/map/data', {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
         },
     }).then(response => {
         return response.json();
-    }).then(geoJson => {
-        mapChart.hideLoading();
-        global.echarts.registerMap('countriesMap', geoJson);
-
+    }).then(json => {
         mapChart.setOption({
-            title: {
-                text: 'GLPI references by country',
-                left: 'center',
-            },
-            tooltip: {
-                formatter: '{b}: {c}'
-            },
-            visualMap: {
-                show: false,
-                min: 1,
-                max: 1000,
-                inRange: {
-                    color: [
-                        '#e6f7ff',
-                        '#bae7ff',
-                        '#91d5ff',
-                        '#69c0ff',
-                        '#40a9ff',
-                        '#1890ff',
-                        '#096dd9',
-                        '#0050b3',
-                        '#003a8c',
-                        '#002766',
-                        '#00134d'
-                    ]
-                },
-            },
             series: [
                 {
-                    type: 'map',
-                    roam: false,
-                    map: 'countriesMap',
-                    emphasis: {
-                        label: {
-                            show: false
-                        }
-                    },
-                    select: {
-                        disabled: true,
-                    },
-                    data: []
+                    data: json
                 }
             ]
         });
-
-        fetch('reference/map/data', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        }).then(response => {
-            return response.json();
-        }).then(json => {
-            mapChart.setOption({
-                series: [
-                    {
-                        data: json
-                    }
-                ]
-            });
-        }).catch(error => {
-            console.error('an error occured: ', error);
-        });
-    }).catch(error => {
-        console.error('an error occured: ', error);
-    });
-
-    // Display the map in a modal when clicking on the maximize button
-    mapChartContainer.parentElement.querySelector('button').addEventListener('click', displayMapInModal);
-
-    // Fix size on window resize
-    window.addEventListener('resize', () => {
-        mapChart.resize();
     });
 });
 
-window.addEventListener("DOMContentLoaded", () => {
-    const data = global.referenceData;
+// Display the map in a modal when clicking on the maximize button
+mapChartContainer.parentElement.querySelector('button').addEventListener('click', displayMapInModal);
 
-    const translator = new Intl.DisplayNames(
-        ['en'],
+// Fix size on window resize
+window.addEventListener('resize', () => {
+    mapChart.resize();
+});
+
+// Render the references list
+const data = global.referenceData;
+
+const translator = new Intl.DisplayNames(
+    ['en'],
+    {
+        type: 'region'
+    }
+);
+const namesMap = new Map();
+const grid = new gridjs.Grid({
+    columns: [
         {
-            type: 'region'
-        }
-    );
-    const namesMap = new Map();
-    const grid = new gridjs.Grid({
-        columns: [
-            {
-                name: "Name",
-                formatter: (cell, row) => {
-                    const url = row.cells[6].data;
-                    const name = cell;
-                    try {
-                        new URL(url);
-                        return gridjs.h(
-                            'a',
-                            {
-                                href: url,
-                                target: '_blank',
-                            },
-                            name
-                        );
-                    } catch (e) {
-                        return name;
+            name: "Name",
+            formatter: (cell, row) => {
+                const url = row.cells[6].data;
+                const name = cell;
+                try {
+                    new URL(url);
+                    return gridjs.h(
+                        'a',
+                        {
+                            href: url,
+                            target: '_blank',
+                        },
+                        name
+                    );
+                } catch (e) {
+                    return name;
+                }
+            }
+        },
+        {
+            name: "Country",
+            formatter: (cell) => {
+                if (cell === null || cell === '') {
+                    return '';
+                }
+
+                if (!namesMap.has(cell)) {
+                    namesMap.set(cell, translator.of(cell.toUpperCase()));
+                }
+                return gridjs.html(`<span class="fi fi-${cell}" title="${namesMap.get(cell)}"></span>`);
+            },
+            sort: {
+                compare: (a, b) => {
+                    if (!namesMap.has(a)) {
+                        namesMap.set(a, translator.of(a.toUpperCase()));
                     }
+                    if (!namesMap.has(b)) {
+                        namesMap.set(b, translator.of(b.toUpperCase()));
+                    }
+                    return namesMap.get(a).localeCompare(namesMap.get(b));
                 }
             },
-            {
-                name: "Country",
-                formatter: (cell) => {
-                    if (cell === null || cell === '') {
-                        return '';
-                    }
-
-                    if (!namesMap.has(cell)) {
-                        namesMap.set(cell, translator.of(cell.toUpperCase()));
-                    }
-                    return gridjs.html(`<span class="fi fi-${cell}" title="${namesMap.get(cell)}"></span>`);
-                },
-                sort: {
-                    compare: (a, b) => {
-                        if (!namesMap.has(a)) {
-                            namesMap.set(a, translator.of(a.toUpperCase()));
-                        }
-                        if (!namesMap.has(b)) {
-                            namesMap.set(b, translator.of(b.toUpperCase()));
-                        }
-                        return namesMap.get(a).localeCompare(namesMap.get(b));
-                    }
-                },
-            },
-            "Nb of assets",
-            "Nb of helpdesk",
-            "Registration date",
-            "Comment",
-            {
-                name: "URL",
-                hidden: true,
-            },
-        ],
-        pagination: {
-            limit: 20
         },
-        search: true,
-        sort: true,
-        data: data,
-    });
-
-    grid.render(document.getElementById('references-table-container'));
+        "Nb of assets",
+        "Nb of helpdesk",
+        "Registration date",
+        "Comment",
+        {
+            name: "URL",
+            hidden: true,
+        },
+    ],
+    pagination: {
+        limit: 20
+    },
+    search: true,
+    sort: true,
+    data: data,
 });
+
+grid.render(document.getElementById('references-table-container'));
